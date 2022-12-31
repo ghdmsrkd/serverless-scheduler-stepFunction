285dc93995a7740c1fb4e78b275aff6840a91b0a
victor <ghdmsrkd@naver.com>
2023-01-01 00:56:30 +0900

    feat: 이벤트 페턴 구조 추가

--- src/controller/scheduler.controller.ts
+++ src/controller/scheduler.controller.ts
@@ -1,1 +1,32 @@
-export class SchedulerController {}
+import { SchedulerService } from '@src/service/scheduler.service';
+
+export class SchedulerController {
+  private eventMap: Record<string, (eventMessage: any) => Promise<any>>;
+  constructor(private schedulerService: SchedulerService) {
+    this.eventMap = {
+      TranslateSchedule: this.translateSchedule.bind(this),
+      GetScheduleStatus: this.getScheduleStatus.bind(this),
+      PublishMessage: this.publishMessage.bind(this),
+    };
+  }
+  getEventHandler(eventPattern: string) {
+    const eventHandler = this.eventMap[eventPattern];
+    if (!eventHandler) {
+      console.error('eventPattern =>', eventPattern);
+      throw new Error('Invalidated Event Pattern!!');
+    }
+    return eventHandler;
+  }
+
+  private async translateSchedule(eventMessage: any) {
+    return this.schedulerService.translateSchedule();
+  }
+
+  private async getScheduleStatus(eventMessage: any) {
+    return this.schedulerService.getScheduleStatus();
+  }
+
+  private async publishMessage(eventMessage: any) {
+    return this.schedulerService.publishMessage();
+  }
+}
